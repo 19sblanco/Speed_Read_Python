0bad35aa5d99828c5fceee7d394b3c458760014f
u1375369 <u1375369@umail.utah.edu>
2024-07-11 19:18:31 -0600

    First commit

---
+++ Main.py
@@ -0,0 +1,65 @@
+import Resources as r
+import Settings as s
+
+
+pause = False
+READMEANU_HEIGHT = 6
+
+
+def main():
+    settings = s.Settings()
+    loadSettings()
+    displayMainMenu()
+
+
+
+def loadSettings():
+    pass
+
+
+def displayMainMenu():
+    pass
+
+
+def error(message):
+    pass
+
+
+def displayCollectionMenu():
+    pass
+
+
+def grabOptionName(option, text):
+    pass
+
+
+def getLineNum(subString, s, delimeter):
+    pass
+
+
+def getInput(pattern, errorMessage):
+    pass
+
+
+def clearConsoleRead(start):
+    pass
+
+
+def changeSpeed(factor):
+    pass
+
+
+def getInputRead():
+    pass
+
+
+def displaySaveSettings():
+    pass
+
+
+def readBook(bookFilePath):
+    pass
+
+
+
+main()
---
+++ Resources.py
@@ -0,0 +1,155 @@
+MAIN_MENU = """
+░██████╗██████╗░███████╗███████╗██████╗░██████╗░███████╗░█████╗░██████╗░
+██╔════╝██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗
+╚█████╗░██████╔╝█████╗░░█████╗░░██║░░██║██████╔╝█████╗░░███████║██║░░██║
+░╚═══██╗██╔═══╝░██╔══╝░░██╔══╝░░██║░░██║██╔══██╗██╔══╝░░██╔══██║██║░░██║
+██████╔╝██║░░░░░███████╗███████╗██████╔╝██║░░██║███████╗██║░░██║██████╔╝
+╚═════╝░╚═╝░░░░░╚══════╝╚══════╝╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░
+                                                Created by Steven Blanco
+    0) Exit
+    1) Read (Choose where to start)
+    2) Read (Begin where you left off)
+    3) Settings
+"""
+
+COLLECTION_MENU = """
+█▀█ █ █▀▀ █▄▀   ▄▀█   █▀▀ █▀█ █░░ █░░ █▀▀ █▀▀ ▀█▀ █ █▀█ █▄░█
+█▀▀ █ █▄▄ █░█   █▀█   █▄▄ █▄█ █▄▄ █▄▄ ██▄ █▄▄ ░█░ █ █▄█ █░▀█
+    0) Go back
+    1) Book of Mormon
+    2) Bible Old Testament
+    3) Bible New Testament
+"""
+
+BOOKEOFMORMON_MENU = """
+█▄▄ █▀█ █▀█ █▄▀   █▀█ █▀▀   █▀▄▀█ █▀█ █▀█ █▀▄▀█ █▀█ █▄░█
+█▄█ █▄█ █▄█ █░█   █▄█ █▀░   █░▀░█ █▄█ █▀▄ █░▀░█ █▄█ █░▀█
+    0)  back
+    1)  THE FIRST BOOK OF NEPHI HIS REIGN AND MINISTRY
+    2)  THE SECOND BOOK OF NEPHI
+    3)  THE BOOK OF JACOB
+    4)  THE BOOK OF ENOS
+    5)  THE BOOK OF JAROM
+    6)  THE BOOK OF OMNI
+    7)  THE WORDS OF MORMON
+    8)  THE BOOK OF MOSIAH
+    9)  THE BOOK OF ALMA
+    10) THE BOOK OF HELAMAN
+    11) THIRD BOOK OF NEPHI
+    12) FOURTH NEPHI
+    13) THE BOOK OF MORMON
+    14) THE BOOK OF ETHER
+    15) THE BOOK OF MORONI
+"""
+
+READ_MENU = """
+█▀█ █▀▀ ▄▀█ █▀▄ █                       w to speed up || s to slow down 
+█▀▄ ██▄ █▀█ █▄▀ ▄                       p to pause and unpause
+                                        q to exit to main menu
+"""
+
+OLDTESTAMENT_MENU = """
+█▀█ █░░ █▀▄   ▀█▀ █▀▀ █▀ ▀█▀ ▄▀█ █▀▄▀█ █▀▀ █▄░█ ▀█▀
+█▄█ █▄▄ █▄▀   ░█░ ██▄ ▄█ ░█░ █▀█ █░▀░█ ██▄ █░▀█ ░█░
+    0)  Go back
+    1)  The First Book of Moses:  Called Genesis                          
+    2)  The Second Book of Moses:  Called Exodus                          
+    3)  The Third Book of Moses:  Called Leviticus                        
+    4)  The Fourth Book of Moses:  Called Numbers                         
+    5)  The Fifth Book of Moses:  Called Deuteronomy                      
+    6)  The Book of Joshua                                               
+    7)  The Book of Judges                                               
+    8)  The Book of Ruth                                                 
+    9)  The First Book of Samuel                                         
+    10) The Second Book of Samuel                                        
+    11) The First Book of the Kings                                      
+    12) The Second Book of the Kings                                     
+    13) The First Book of the Chronicles                                 
+    14) The Second Book of the Chronicles                                
+    15) Ezra                                                             
+    16) The Book of Nehemiah                                             
+    17) The Book of Esther                                               
+    18) The Book of Job                                                  
+    19) The Book of Psalms                                               
+    20) The Proverbs
+    21) Ecclesiastes
+    22) The Song of Solomon
+    23) The Book of the Prophet Isaiah
+    24) The Book of the Prophet Jeremiah
+    25) The Lamentations of Jeremiah
+    26) The Book of the Prophet Ezekiel
+    27) The Book of Daniel
+    28) Hosea
+    29) Joel
+    30) Amos
+    31) Obadiah
+    32) Jonah
+    33) Micah
+    34) Nahum
+    35) Habakkuk
+    36) Zephaniah
+    37) Haggai
+    38) Zechariah
+    39) Malachi
+"""
+
+NEWTESTAMENT_MENU = """
+█▄░█ █▀▀ █░█░█   ▀█▀ █▀▀ █▀ ▀█▀ ▄▀█ █▀▄▀█ █▀▀ █▄░█ ▀█▀
+█░▀█ ██▄ ▀▄▀▄▀   ░█░ ██▄ ▄█ ░█░ █▀█ █░▀░█ ██▄ █░▀█ ░█░
+    0)  back
+    1)  The New Testament of the King James Bible                        
+    2)  The Gospel According to Saint Matthew                            
+    3)  The Gospel According to Saint Mark                               
+    4)  The Gospel According to Saint Luke                               
+    5)  The Gospel According to Saint John                               
+    6)  The Acts of the Apostles                                         
+    7)  The Epistle of Paul the Apostle to the Romans                    
+    8)  The First Epistle of Paul the Apostle to the Corinthians         
+    9)  The Second Epistle of Paul the Apostle to the Corinthians
+    10) The Epistle of Paul the Apostle to the Galatians
+    11) The Epistle of Paul the Apostle to the Ephesians
+    12) The Epistle of Paul the Apostle to the Philippians
+    13) The Epistle of Paul the Apostle to the Colossians
+    14) The First Epistle of Paul the Apostle to the Thessalonians
+    15) The Second Epistle of Paul the Apostle to the Thessalonians
+    16) The First Epistle of Paul the Apostle to Timothy
+    17) The Second Epistle of Paul the Apostle to Timothy
+    18) The Epistle of Paul the Apostle to Titus
+    19) The Epistle of Paul the Apostle to Philemon
+    20) The Epistle of Paul the Apostle to the Hebrews
+    21) The General Epistle of James
+    22) The First Epistle General of Peter
+    23) The Second General Epistle of Peter
+    24) The First Epistle General of John
+    25) The Second Epistle General of John
+    26) The Third Epistle General of John
+    27) The General Epistle of Jude
+    28) The Revelation of Saint John the Divine
+"""
+
+
+
+
+EXPLAINSETTINGS_MENU = """
+Speed:
+    The number of milliseconds that a line will display for 
+    ***
+    1000 milliseconds are in  1 second, meaning...
+    500  milliseconds is  1/2 a second, and
+    5000 milliseconds is  5   seconds
+    
+Page size:  
+    This determines how many vertical lines of space in which text will print down (page size)
+    ***
+    Page Size = 1: display each line of text on the same line.
+    Page Size = 10: starting at the top of the window, display the
+    first line of text. Next frame display the second line of 
+    text a line below the first. Simularly the third will print under the
+    second and so on, continuing to 10. After the 10th, the next line will print
+    at the top of the page repeating the cycle.
+Revert to default:  
+    Reverts the settings to their default values and removes bookmark
+    ***
+    speed = 5000
+    Page size = 10
+"""
