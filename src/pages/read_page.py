6d53087842028576d0b55ff100120b90d83f1ee0
u1375369 <u1375369@umail.utah.edu>
2024-07-22 21:23:01 -0600

    added primitive version of read page

--- src/pages/read_page.py
+++ src/pages/read_page.py
@@ -1,10 +1,30 @@
 from pages.page import page
+import time
 
-class read_page():
+class read_page(page):
+    __READ_MENU = """
+██████╗ ███████╗ █████╗ ██████╗ ██╗
+██╔══██╗██╔════╝██╔══██╗██╔══██╗██║
+██████╔╝█████╗  ███████║██║  ██║██║
+██╔══██╗██╔══╝  ██╔══██║██║  ██║╚═╝
+██║  ██║███████╗██║  ██║██████╔╝██╗
+╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝
+"""
     __main_menu = None
 
     def __init__(self, text, pointer, main_menu):
+        super().__init__()
         self.__main_menu = main_menu
-        print("in read_page")
-        print(pointer)
-        print(text[pointer:pointer+500])
+        print(self.__READ_MENU)
+        # TODO: load settings
+        lines = text.split("\n")
+        num_lines = 0
+        for i in range(len(lines)):
+            print(lines[i])
+            num_lines += 1
+            time.sleep(2)
+            if num_lines == 10:
+                num_lines = 0
+                super().clear_console()
+                print(self.__READ_MENU)
+
