666046ce83aeef5074f0b926546c87b90fd869c1
u1375369 <u1375369@umail.utah.edu>
2024-07-30 17:07:01 -0600

    added a save feature to settings

--- src/pages/settings_page.py
+++ src/pages/settings_page.py
@@ -1,6 +1,18 @@
+import json
+import time
+
 from menu_option import menu_option
 from pages.page import page
 
+
+"""
+TODO
+allow user to 
+    * change settings
+    * save settings
+when loading settings object, load save file settings
+? is it possible to only have this program edit that file?
+"""
 
 class settings_page(page):
     __SETTINGS_MENU = """
@@ -10,10 +22,12 @@ class settings_page(page):
 ╚════██║██╔══╝     ██║      ██║   ██║██║╚██╗██║██║   ██║╚════██║
 ███████║███████╗   ██║      ██║   ██║██║ ╚████║╚██████╔╝███████║
 ╚══════╝╚══════╝   ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
-                                                                """
+"""
 
     __main_menu = None
     __menu_options = None
+
+    __speed = 5
 
 
     def __init__(self, main_menu):
@@ -25,6 +39,20 @@ class settings_page(page):
         super().display_options(self.__menu_options)
         user_input = super().get_input(len(self.__menu_options))
         super().handle_input(user_input, self.__menu_options)
+
+        self.save_settings()
+
+
+    def save_settings(self):
+        data = {
+            "speed": self.__speed,
+        }
+
+        with open("save.txt", "w") as save_file:
+            json.dump(data, save_file, indent=4)
+        print("save complete...")
+        time.sleep(1)
+        self.__init__(self.__main_menu)
 
 
     def create_options(self):
@@ -43,5 +71,6 @@ class settings_page(page):
 
 
     def change_speed(self):
-        print("change_speed")
-        pass
+        print("\nCurrent speed:", str(self.__speed))
+        max_speed = 100
+        self.__speed = super().get_input(max_speed)
