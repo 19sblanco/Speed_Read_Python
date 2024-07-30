f775cae4dd452f0745c1a255e73a17ca85043181
u1375369 <u1375369@umail.utah.edu>
2024-07-30 17:35:12 -0600

    added sigint handler for cleaner exiting

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
+import signal
+import os
+import platform
 from pages.main_menu import main_menu 
+
+"""
+handles the ctrl-c signal
+"""
+def sigint_handler(sig, frame):
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+    print("GoodBye!")
+    exit(0)
 
 """
 project plans in file todo and specifications.txt
 """
 
 def main():
+    signal.signal(signal.SIGINT, sigint_handler)
     mm = main_menu()
 
 
