91a2776fd5455858bc1ec69e6da0ef9d735878f1
u1375369 <u1375369@umail.utah.edu>
2024-08-06 16:53:46 -0600

    online content page can retrieve online content

--- src/pages/online_content_page.py
+++ src/pages/online_content_page.py
@@ -1,5 +1,8 @@
 from menu_option import menu_option
 from pages.page import page
+import requests
+from bs4 import BeautifulSoup
+import time
 
 
 class online_content_page(page):
@@ -17,7 +20,7 @@ class online_content_page(page):
 ██║     ██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██║╚██╗██║   ██║   
 ╚██████╗╚██████╔╝██║ ╚████║   ██║   ███████╗██║ ╚████║   ██║   
  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═══╝   ╚═╝   
-                                                               """
+"""
 
     __main_menu = None
     __menu_options = None
@@ -48,8 +51,44 @@ class online_content_page(page):
         )
         self.__menu_options.append(
             menu_option(
-                "Add New by URL",
-                self.__main_menu
+                "Add New Content by URL",
+                self.add_by_url
             )
         )
 
+    def add_by_url(self):
+        while True:
+            try:
+                url = input("Enter a url: ")
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, 'html.parser')
+
+                break
+            except:
+                print("Invalid url")
+        
+        # print <h1>
+        h1 = soup.h1.get_text()
+        print()
+        print(h1)
+        print()
+        
+        # print all <p>
+        all_ps = soup.find_all('p')
+        for p in all_ps:
+            print(p.get_text())
+
+
+
+
+
+"""
+Please enter a url: <url>
+
+process the url with beautiful soup and get
+    * <h1>
+    * <p>
+format text 
+    * make text 70 wide
+save text in file named <h1> "replace space with '_'"
+"""
