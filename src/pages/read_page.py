043c2ecd5a19cf4c743ec216d54b886435f75883
u1375369 <u1375369@umail.utah.edu>
2024-09-03 16:04:39 -0600

    working on event handling in read_page.py

--- src/pages/read_page.py
+++ src/pages/read_page.py
@@ -2,28 +2,34 @@ from pages.page import page
 import time
 import os
 import json
+from pynput import keyboard
+
 
 class read_page(page):
     __READ_MENU = """
-██████╗ ███████╗ █████╗ ██████╗ ██╗
-██╔══██╗██╔════╝██╔══██╗██╔══██╗██║
-██████╔╝█████╗  ███████║██║  ██║██║
+██████╗ ███████╗ █████╗ ██████╗ ██╗         
+██╔══██╗██╔════╝██╔══██╗██╔══██╗██║         ↑ to speed up 
+██████╔╝█████╗  ███████║██║  ██║██║         ↓ to slow down
 ██╔══██╗██╔══╝  ██╔══██║██║  ██║╚═╝
 ██║  ██║███████╗██║  ██║██████╔╝██╗
 ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝
 """
     __main_menu = None
     __lines = None
-    __start_idx = None
+    __curr_idx = None
     __lines_to_display = []
     __SAVE_FILE = "save.json"
     __speed = 5 # default speed
+    __listener = None
 
 
     def __init__(self, lines, start_idx, main_menu):
+        with keyboard.Listener(on_press=self.change_speed) as listener:
+            listener.join()
+
         super().__init__()
         self.__lines = lines
-        self.__start_idx = start_idx
+        self.__curr_idx = start_idx
         self.__main_menu = main_menu
         print(self.__READ_MENU)
         self.load_settings()
@@ -31,7 +37,8 @@ class read_page(page):
 
 
     def display(self):
-        for i in range(self.__start_idx, len(self.__lines)):
+        for i in range(self.__curr_idx, len(self.__lines)):
+            self.__curr_idx = i
             self.__lines_to_display.append(self.__lines[i])
             super().clear_console()
             print(self.__READ_MENU)
@@ -44,13 +51,19 @@ class read_page(page):
                 self.__lines_to_display = []
                 self.__lines_to_display.append(last_line)
 
-    
-    def increase_speed(self):
-        pass
+
+    def change_speed(self, key):
+        try:
+            if key.char == 'up':
+                print("Key Up pressed")
+                self.__speed *= 1.1
+                self.__display()
+            elif key.char == 'down':
+                print("Key Down pressed")
+        except AttributeError:
+            pass       
 
 
-    def decrease_speed(self):
-        pass
 
 
     def load_settings(self):
@@ -64,6 +77,7 @@ class read_page(page):
             with open(self.__SAVE_FILE, "w") as save_file:
                 json.dump(data, save_file, indent=4)
             self.__init__(self.__lines, self.__start_idx, self.__main_menu)
+
        
 
 
